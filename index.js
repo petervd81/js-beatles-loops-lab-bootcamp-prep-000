9e5204013a170793ff9a7142214613afd0dc2b37
Peter van Dijk <peter@petervandijk.net>
2018-05-19 19:17:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(musicians, instruments) {
-  var plays = new Array;
+  var plays = new Array();
   for (var i = 0; i < musicians.length; i++) {
     plays.push(`${musicians[i]} plays ${instruments[i]}`);
   }
